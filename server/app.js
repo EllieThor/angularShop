74a0448dc64115b072b1a31e45e36e341db68dc2
EllieThor <elliethor18@gmail.com>
2021-08-11 19:23:58 +0300

    add nodejs db and folders

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./utils/database");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+const UsersRoute = require("./routs/UsersRoute.js");
+app.use("/users", UsersRoute);
+
+const ProductsRoute = require("./routs/ProductsRoute.js");
+app.use("/products", ProductsRoute);
+
+const CartsRoute = require("./routs/CartsRoute.js");
+app.use("/carts", CartsRoute);
+
+app.use((req, res) => {
+  res.send("Page NotFound");
+});
+
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(5001);
+    console.log("Connected DB !!");
+  })
+  .catch((err) => {
+    console.log("Error connected DB !!", err);
+  });
