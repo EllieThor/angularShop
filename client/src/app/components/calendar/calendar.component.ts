a45b41af3cdb45fc8ab9e12ed02fd1642157f5b7
EllieThor <elliethor18@gmail.com>
2021-09-19 18:34:43 +0300

    calendar work well the days with more than 2 deliveris in red and funciunalety work well. needed to be in shipping-details component

--- client/src/app/components/calendar/calendar.component.ts
+++ client/src/app/components/calendar/calendar.component.ts
@@ -70,19 +70,6 @@ export class CalendarComponent implements OnInit {
 
     await this.ordersService.getOrdersDates('/orders/getOrdersDates');
 
-    // this.ordersService._datesArr.ordersCount.map((date: any) => {
-    //   new Date().toISOString().split('T').shift() ===
-    //   date.ShippingDate.split('T').shift()
-    //     ? console.log('yes: ', date.ShippingDate.split('T').shift())
-    //     : console.log('no: ', date.ShippingDate.split('T').shift());
-    // });
-
-    this.ordersService._datesArr.ordersCount.map((date: any) => {
-      this.isShippingAvailable(date.ShippingDate)
-        ? console.log('yes: ', date.ShippingDate.split('T').shift())
-        : console.log('no: ', date.ShippingDate.split('T').shift());
-    });
-
     this.lastDay = new Date(
       this._date.getFullYear(),
       this._date.getMonth() + 1,
@@ -105,29 +92,54 @@ export class CalendarComponent implements OnInit {
 
     this.nextDays = 7 - this.lastDayIndex - 1;
     this._currentMonthIndex = this._date.getMonth();
-    console.log('aa: ', this._currentMonthIndex);
     this._currentDateStr = new Date().toDateString();
 
     // print last month days
     for (let x = this.firstDayIndex; x > 0; x--) {
       // if this._currentMonthIndex=0 => january- last month needed to be 11=> december and not -1
+
+      //   d.ShippingDate.split('T').shift().split('-') ['2021', '09', '19']
+      // this._currentDateStr.split(' ')  ['Sun', 'Sep', '19', '2021']
+      let shippingNum = this.ordersService._datesArr.ordersCount.filter(
+        (d: any) =>
+          d.ShippingDate.split('T').shift().split('-')[0] ==
+            this._currentDateStr.split(' ')[3] &&
+          d.ShippingDate.split('T').shift().split('-')[1] ==
+            this._currentMonthIndex &&
+          d.ShippingDate.split('T').shift().split('-')[2] ==
+            this.prevLastDay - x + 1
+      );
+
       if (this._currentMonthIndex != 0) {
         this._monthDays.push({
           type: 0,
           num: this.prevLastDay - x + 1,
           monthIndex: this._currentMonthIndex - 1,
+          availability: shippingNum.length > 2 ? false : true,
         });
       } else {
         this._monthDays.push({
           type: 0,
           num: this.prevLastDay - x + 1,
           monthIndex: 11,
+          availability: shippingNum.length > 2 ? false : true,
         });
       }
     }
 
     // print current month days
     for (let i = 1; i <= this.lastDay; i++) {
+      //   d.ShippingDate.split('T').shift().split('-') ['2021', '09', '19']
+      // this._currentDateStr.split(' ')  ['Sun', 'Sep', '19', '2021']
+      let shippingNum = this.ordersService._datesArr.ordersCount.filter(
+        (d: any) =>
+          d.ShippingDate.split('T').shift().split('-')[0] ==
+            this._currentDateStr.split(' ')[3] &&
+          d.ShippingDate.split('T').shift().split('-')[1] ==
+            this._currentMonthIndex + 1 &&
+          d.ShippingDate.split('T').shift().split('-')[2] == i
+      );
+
       if (
         i === new Date().getDate() &&
         this._date.getMonth() === new Date().getMonth()
@@ -136,12 +148,14 @@ export class CalendarComponent implements OnInit {
           type: 1,
           num: i,
           monthIndex: this._currentMonthIndex,
+          availability: shippingNum.length > 2 ? false : true,
         });
       } else {
         this._monthDays.push({
           type: 3,
           num: i,
           monthIndex: this._currentMonthIndex,
+          availability: shippingNum.length > 2 ? false : true,
         });
       }
     }
@@ -149,53 +163,58 @@ export class CalendarComponent implements OnInit {
     // print next month days
     for (let j = 1; j <= this.nextDays; j++) {
       // if this._currentMonthIndex=11 => december- next month needed to be 0=> january and not 12
+
+      //   d.ShippingDate.split('T').shift().split('-') ['2021', '09', '19']
+      // this._currentDateStr.split(' ')  ['Sun', 'Sep', '19', '2021']
+      let shippingNum = this.ordersService._datesArr.ordersCount.filter(
+        (d: any) =>
+          d.ShippingDate.split('T').shift().split('-')[0] ==
+            this._currentDateStr.split(' ')[3] &&
+          d.ShippingDate.split('T').shift().split('-')[1] ==
+            this._currentMonthIndex + 1 &&
+          d.ShippingDate.split('T').shift().split('-')[2] == j
+      );
+
       if (this._currentMonthIndex != 11) {
         this._monthDays.push({
           type: 2,
           num: j,
           monthIndex: this._currentMonthIndex + 1,
+          availability: shippingNum.length > 2 ? false : true,
         });
       } else {
         this._monthDays.push({
           type: 2,
           num: j,
           monthIndex: 0,
+          availability: shippingNum.length > 2 ? false : true,
         });
       }
     }
     console.log('this._monthDays ARR: ', this._monthDays);
   }
 
-  // isShippingAvailable(date.ShippingDate)
-  isShippingAvailable(date: string) {
-    if (
-      new Date().toISOString().split('T').shift() === date.split('T').shift()
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   prevIconClicked() {
     this._date.setMonth(this._date.getMonth() - 1);
-
     this.renderCalendar();
   }
 
   nextIconClicked() {
     this._date.setMonth(this._date.getMonth() + 1);
-    console.log('nextIconClicked: ', this._date.getMonth() + 1);
     this.renderCalendar();
-  }
-
-  someDayClicked(dayOb: any) {
-    console.log('some day clicked: ', dayOb);
   }
 
   backToTodayClicked() {
     this._date = new Date();
-    console.log('backToTodayClicked : ', this._date);
     this.renderCalendar();
+  }
+
+  someDayClicked(dayOb: any) {
+    // availability is false if there is more then 2 deliveries for this day
+    if (dayOb.availability) {
+      console.log('some day clicked: availability true ', dayOb);
+    } else {
+      console.log('some day clicked: availability false ', dayOb);
+    }
   }
 }
